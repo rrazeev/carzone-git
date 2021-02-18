ab3a8105532be521f6cb1af4852b0c199707362b
rrazeev <lochan50@gmail.com>
2021-02-18 16:46:22 +0530

    added upto contacts page

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
+
 def home(request):
-    return  render(request,'pages/home.html')
+    return render(request, 'pages/home.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+
+def services(request):
+    return render(request, 'pages/services.html')
+
+
+def contact(request):
+    return render(request, 'pages/contact.html')
