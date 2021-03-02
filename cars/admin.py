bc2b48a0f1f770de82aff5720a1d5fa1dfdf9efc
rrazeev <lochan50@gmail.com>
2021-03-02 13:32:00 +0530

    car database fetching done

---
+++ cars/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Car
+from django.utils.html import format_html
+
+
+class CarAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40" style="border-radius: 50px;" />'.format(object.car_photo.url))
+    thumbnail.short_description = "car Image"
+
+    list_display = ('id', 'car_title','thumbnail', 'city', 'color', 'model', 'year',  'body_style', 'fuel_type', 'is_featured')
+    list_display_links = ('id', 'thumbnail','car_title')
+    list_editable = ('is_featured',)
+    search_fields = ('id', 'car_title', 'city')
+    list_filter = ('city','color','model')
+
+
+admin.site.register(Car, CarAdmin)
